8d97446889648cb6857c5eab1a1d305a18161072
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2024-10-21 17:59:18 +0200

    fixing calculation errors and adding validation function to check parameters units and type

--- index.js
+++ index.js
@@ -10,20 +10,41 @@ const velocityinKmPerHour = 10000; // velocity (km/h)
 const accelerationInMetersPerSecSquared = 3; // acceleration (m/s^2)
 const timeInSeconds = 3600; // seconds (1 hour)
 const distanceInKm = 0; // distance (km)
-const remaineingfuelInKg = 5000; // remaining fuel (kg)
-const fuelBurnRateInKgPerSecond = 0.5; // fuel burn rate (kg/s)
+const remainaingfuelInKg = 5000; // remaining fuel (kg)
+const fuelBurnRateInKgPerSec = 0.5; // fuel burn rate (kg/s)
 
+//unit conversion
+const velocityinMetersPerSec = velocityinKmPerHour / 3.6 //velocity (km/s)
 
-const updatedDistance = distanceInKm + (velocityinKmPerHour * timeInSeconds ) //calcultes new distance
-const updatedRemainingFuel = fuelBurnRateInKgPerSecond * timeInSeconds  //calculates remaining fuel
+const updatedDistance = distanceInKm + (velocityinKmPerHour * (timeInSeconds / 3600)) //calcultes new distance
+const updatedRemainingFuel = remainaingfuelInKg - (fuelBurnRateInKgPerSec * timeInSeconds) //calculates remaining fuel
 
 // Pick up an error with how the function below is called and make it robust to such errors
+const validateUnits = (velocity, acceleration, time)=>{
+  if (typeof velocity !== 'number' || velocity < 0) {
+    throw new Error('Invalid velocity: must be a positive number (in km/s)');
+  }
+  if (typeof acceleration !== 'number' || acceleration < 0) {
+    throw new Error('Invalid acceleration: must be a positive number(in m/s²)');
+  }
+  if (typeof time !== 'number' || time < 0) {
+    throw new Error('Invalid time: must be a positive number (in seconds)');
+  }
+}
+
 const calcNewVelocity = (velocity, acceleration , time) => { 
+ validateUnits(velocity, acceleration, time)
   return velocity + (acceleration * time)
 }
-const updatedVelocity = calcNewVelocity( velocityinKmPerHour, accelerationInMetersPerSecSquared, timeInSeconds ) //calculates new velocity based on acceleration
 
-console.log(`Corrected New Velocity: ${updatedVelocity} km/h`);
+try {
+  const updatedVelocity = calcNewVelocity(velocityinMetersPerSec, accelerationInMetersPerSecSquared, timeInSeconds);
+  const updatedVelocityInKmH = updatedVelocity * 3.6
+  console.log(`Corrected New Velocity: ${updatedVelocityInKmH} km/h`);
+} catch (error) {
+  console.error(error.message);
+}
+
 console.log(`Corrected New Distance: ${updatedDistance} km`);
 console.log(`Corrected Remaining Fuel: ${updatedRemainingFuel} kg`);
 
